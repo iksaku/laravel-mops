a58839a5b28523f32eb8266e236d549ba36da38a
Jorge González <yo@jorgeglz.io>
2020-10-16 19:29:19 -0500

    Fixed card + form section stuff

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'w-full max-w-lg bg-white px-4 sm:px-6 py-4 sm:rounded shadow space-y-4 overflow-y-hidden']) }}>
+<div {{ $attributes->merge(['class' => 'w-full md:max-w-lg bg-white px-4 sm:px-6 py-4 sm:rounded shadow space-y-4 overflow-y-hidden']) }}>
     {{ $slot }}
 </div>
--- resources/views/components/form/section.blade.php
+++ resources/views/components/form/section.blade.php
@@ -1,7 +1,9 @@
 @props(['title', 'description' => null, 'centeredTitle' => false])
 
-<div class="relative w-full flex flex-col md:flex-row items-start md:space-x-4 space-y-4 md:space-y-0 py-4 first:pt-0 last:pb-0">
-    <div class="md:sticky inset-x-0 top-0 w-full md:w-1/3 text-center md:text-left space-y-2">
+<div
+    {{ $attributes->merge(['class' => 'relative w-full flex flex-col md:flex-row items-start md:space-x-4 space-y-4 md:space-y-0 py-4 first:pt-0 last:pb-0']) }}
+>
+    <div class="md:sticky inset-x-0 top-0 w-full md:w-1/3 flex-shrink-0 text-center md:text-left space-y-2">
         <div class="text-lg font-medium">
             {{ $title }}
         </div>
@@ -12,8 +14,8 @@
         @endisset
     </div>
 
-    <div class="w-full flex-grow">
-        <x-mops::card {{ $attributes }}>
+    <div class="w-full md:max-w-lg flex-grow">
+        <x-mops::card>
             {{ $slot }}
         </x-mops::card>
     </div>
