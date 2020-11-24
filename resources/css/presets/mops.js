79a1608d553f74385d588f869c44635dd0d0e5bd
Jorge González <yo@jorgeglz.io>
2020-11-24 15:17:51 -0600

    Use default palette in MOPS Tailwind preset

--- resources/css/presets/mops.js
+++ resources/css/presets/mops.js
@@ -12,7 +12,19 @@ module.exports = {
     theme: {
         colors: {
             inherit: 'inherit',
-            ...colors
+            transparent: 'transparent',
+            current: 'currentColor',
+
+            black: colors.black,
+            white: colors.white,
+            gray: colors.coolGray,
+            red: colors.red,
+            yellow: colors.amber,
+            green: colors.emerald,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            purple: colors.violet,
+            pink: colors.pink,
         }
     },
 
