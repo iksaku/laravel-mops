f149006fda81955feeb5c7c7286716fa9c95d10d
Jorge González <yo@jorgeglz.io>
2020-11-25 17:35:45 -0600

    Oops, need nesting

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -4,38 +4,40 @@
 
 <div class="w-full -my-2 py-2">
     <x-mops::card
-        {{ $attributes->merge(['class' => 'w-full flex flex-col divide-y divide-gray-200']) }}
+        {{ $attributes->merge(['class' => 'w-full']) }}
         :stretch="true"
     >
-        @isset($actions)
-            {{-- Actions are items placed inside the table card, but above the table --}}
-            <div class="px-6 py-4">
-                {{ $actions }}
-            </div>
-        @endisset
-
-        @if($for->isEmpty())
-            {{-- If empty, look for a slot named "empty", otherwise, use a generic one --}}
-            @isset($empty)
-                {{ $empty }}
-            @else
-                <div class="text-center text-gray-500 text-xl px-6 py-3">
-                    No se encontraron registros.
+        <div class="w-full flex flex-col divide-y divide-gray-200">
+            @isset($actions)
+                {{-- Actions are items placed inside the table card, but above the table --}}
+                <div class="px-6 py-4">
+                    {{ $actions }}
                 </div>
             @endisset
-        @else
-            {{-- If not empty, render a table :)--}}
-            <div class="overflow-x-auto">
-                <table class="min-w-full divide-y divide-gray-200">
-                    {{ $slot }}
-                </table>
-            </div>
 
-            @if($for->hasPages())
-                <div class="px-6 py-2">
-                    {{ $for->links() }}
+            @if($for->isEmpty())
+                {{-- If empty, look for a slot named "empty", otherwise, use a generic one --}}
+                @isset($empty)
+                    {{ $empty }}
+                @else
+                    <div class="text-center text-gray-500 text-xl px-6 py-3">
+                        No se encontraron registros.
+                    </div>
+                @endisset
+            @else
+                {{-- If not empty, render a table :)--}}
+                <div class="overflow-x-auto">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        {{ $slot }}
+                    </table>
                 </div>
+
+                @if($for->hasPages())
+                    <div class="px-6 py-2">
+                        {{ $for->links() }}
+                    </div>
+                @endif
             @endif
-        @endif
+        </div>
     </x-mops::card>
 </div>
