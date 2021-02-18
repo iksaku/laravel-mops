879de4b95f8aa83c92d2fa4c7eb8f07be81df42d
Jorge González <yo@jorgeglz.io>
2021-02-17 20:23:38 -0600

    Well, this should not be here...

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,5 @@
 @props(['type' => 'button'])
 
-<button type="{{ $type }}" {{ $attributes->merge(['class' => 'flex items-center font-medium px-4 py-2 rounded-md focus:ring focus:outline-none space-x-2 disabled:opacity-75']) }}>
+<button type="{{ $type }}" {{ $attributes->merge(['class' => 'flex items-center font-medium px-4 py-2 rounded-md focus:ring focus:outline-none disabled:opacity-75']) }}>
     {{ $slot }}
 </button>
